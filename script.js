a8d042afbd4bbdaabc22643a2a22501f541af49e
byprtma <bionpratama@gmail.com>
2024-08-09 22:04:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 onload = () => {
   document.body.classList.remove("container");
 };
+
+// JavaScript untuk menampilkan gambar setelah tombol ditekan
+document.getElementById("myButton").onclick = function() {
+    var img = document.getElementById("myImage");
+    var button = document.getElementById("myButton");
+    var lanjutan = document.getElementById("lanjutan");
+
+    img.style.display = "block"; // Menampilkan gambar
+    button.style.display = "none"; // Menyembunyikan tombol setelah diklik
+
+    setTimeout(function() {
+        lanjutan.style.display = "block"; // Menampilkan konten lanjutan
+    }, 1500); // 2000 milidetik = 2 detik
+};
